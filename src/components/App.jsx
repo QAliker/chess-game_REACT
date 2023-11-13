53f5fe85f920e278589db95a4fb34fea302fc7a5
Quentin <alikerquentin@gmail.com>
2023-11-13 22:40:44 +0100

    created chessboard

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+//Import
+
+//Models(variables)
+const squareNumber = 64
+const horizontalSquares = ["A", "B", "C", "D", "E", "F", "G", "H"]
+const verticalSquares = ["1", "2", "3", "4", "5", "6", "7", "8"]
+const App = () => (
+  <div>
+    <h1>Nombre de case à créer : {squareNumber}</h1>
+    <div className="flex justify-center">
+      <table className="mt-5">
+            <tbody>
+            <td className="border border-sky-400 bg-slate-400 text-4xl px-3"></td>
+            {horizontalSquares.map((letter) => (
+              <td
+                key={letter}
+                className="border border-sky-400 bg-slate-400 text-4xl px-3">
+                {letter}
+              </td>
+            ))}
+          {verticalSquares.map((number) => (
+            <tr key={number}>
+              <td className="border border-sky-400 bg-slate-400 text-4xl px-3">
+                {number}
+              </td>
+              {horizontalSquares.map((letter) => (
+                <td
+                  key={`${letter}-${number}`}
+                  className="border border-sky-400 bg-white text-4xl px-3">{letter} {number}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+)
+//UseContext pour les className similaire
+
+export default App
